612a57affefa210bd7bb1b9f9ba730972f323274
Garius6 <garius642@gmail.com>
2022-10-21 22:36:22 +0300

    Remove .config/nvim/lua/dap_conf.lua Remove .config/nvim/lua/lsp_conf.lua Remove .config/nvim/lua/statusline_conf.lua Remove .config/nvim/lua/telescope_conf.lua

--- dot_config/nvim/lua/dap_conf.lua
+++
@@ -1,8 +0,0 @@
-local dap = require('dap')
-
-
-vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, { desc = "toggle debug breakpoint" })
-vim.keymap.set('n', '<leader>dc', dap.continue, { desc = "continue debug programm executuion" })
-vim.keymap.set('n', '<leader>dn', dap.step_over, { desc = "step over" })
-vim.keymap.set('n', '<leader>dN', dap.step_into, { desc = "step over" })
-vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = "open debug repl" })
